4ad2a05056cd3fc97d8c9630cf31971c9e558244
xbc <464751858@qq.com>
2017-12-15 06:32:07 +0000

    ss

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,24 +7,23 @@
  */
 function get_db_config()
 {
-    if(getenv('IS_IN_HEROKU'))
-    {
-        $url = parse_url(getenv('DATABASE_URL'));
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
         return $db_config = [
-            'connection'    => 'pgsql',
-            'host'  => $url('host'),
-            'database'  => substr($url['path'],1),
-            'username'  => $url['user'],
-            'password'  => $url['pass'],
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
-    }else
-    {
+    } else {
         return $db_config = [
-            'connection'    => env('DB_CONNECTION','mysql'),
-            'host'          => env('DB_HOST','localhost'),
-            'database'      => env('DB_DATABASE','forge'),
-            'username'      => env('DB_USERNAME','forge'),
-            'password'      => env('DB_PASSWORD',''),
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
 }
